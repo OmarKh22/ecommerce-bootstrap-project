92e441549e35c32763d5bc8223079f2052d97ba4
Ziad Nasser <55110484+ziad7411@users.noreply.github.com>
2025-05-02 19:42:06 +0300

    full

--- blogScript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const closeBtn = document.getElementById("closeAnnouncement");
-  const announcementBar = document.getElementById("announcementBar");
-
-  closeBtn.addEventListener("click", function () {
-    announcementBar.style.display = "none";
-  });
-});
